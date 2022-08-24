f14706af61631d7400368ecdef9640e397010f5a
Prince Fefar <pfefar1@gmail.com>
2022-08-23 19:41:41 -0700

    fixed initialize

--- public-gateway/src/Contract.sol
+++ public-gateway/src/Contract.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.10;
 
 contract Gateway {
+    /*//////////////////////////////////////////////////////////////
+                             Constructor
+    //////////////////////////////////////////////////////////////*/
+
     address private owner;
 
     constructor() {
@@ -13,16 +17,21 @@ contract Gateway {
         _;
     }
 
-    bytes masterVerificationKey;
+    /*//////////////////////////////////////////////////////////////
+                             Initialization
+    //////////////////////////////////////////////////////////////*/
 
-    /// @notice Initialize the contract
-    /// @param _masterVerificationKey The first number/message data
-    function setFulfillAllowance(bytes memory _masterVerificationKey)
-        public
-        onlyOwner
-    {
-        masterVerificationKey = _masterVerificationKey;
+    address masterVerificationAddress;
+
+    /// @notice Initialize the verification address
+    /// @param _masterVerificationAddress The input address
+    function initialize(address _masterVerificationAddress) public onlyOwner {
+        masterVerificationAddress = _masterVerificationAddress;
     }
+
+    /*//////////////////////////////////////////////////////////////
+                             Update Routes
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice Updating the Routes
     /// @param _routes List of routes
@@ -33,62 +42,33 @@ contract Gateway {
         bytes32[] memory _verificationKeys,
         uint256 _num
     ) public onlyOwner {
-        
         // ?? are the signature going to be for the individual routes or whole payload
-       
-       
         // Parse out and create the mapping of the values
     }
 
-    /// @notice Pre-Execution 
+    /*//////////////////////////////////////////////////////////////
+                             Pre Execution
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Pre-Execution
     /// @param _handle handle for routing
     /// @param _contractAddress contract address for routing
     /// @param _inputs inputs provided for the execution
     /// @param _callback callback for the sorce destination
     /// @param _signature signature of params
-    function preExecution()
-        public
-        onlyOwner
-    {
+    function preExecution() public onlyOwner {
         // is the signature just for the inputs or any other data attached to it ?
-
-        // what is a _handle ? 
-
+        // what is a _handle ?
     }
 
+    /*//////////////////////////////////////////////////////////////
+                             Post Execution
+    //////////////////////////////////////////////////////////////*/
 
-    /// @notice Post-Execution 
+    /// @notice Post-Execution
     /// @param _outputs Outputs from the private execution
     /// @param _data task ID+input pair
     /// @param _signature signature of params
-    /// @param _sourceNetwork 
-    function postExecution()
-        public
-        onlyOwner
-    {
-
-
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /// @param _sourceNetwork
+    function postExecution() public onlyOwner {}
 }
