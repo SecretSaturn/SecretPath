b47d7a75c19418bf5f70e7388c8f16654164d691
Prince Fefar <pfefar1@gmail.com>
2022-09-12 08:39:54 -0700

    remove import

--- public-gateway/src/Contract.sol
+++ public-gateway/src/Contract.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import "openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import "openzeppelin-contracts/contracts/utils/Counters.sol";
 
 library Util {
