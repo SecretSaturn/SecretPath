068c4ee15ad2a1796de112845737aa346b5dd647
h3rt <alexander.hertlein@icloud.com>
2023-12-21 02:00:02 +0100

    Remove toplevelawait

--- TNLS-Clients/payload-encryption/vite.config.ts
+++ TNLS-Clients/payload-encryption/vite.config.ts
@@ -2,11 +2,10 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [wasm()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
