209d9452581bb18bf78f76762dd21c0adc154b8c
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-05-19 19:11:04 +0200

    update

--- TNLS-Gateways/solana-gateway/programs/solana-gateway/src/lib.rs
+++ TNLS-Gateways/solana-gateway/programs/solana-gateway/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program::{transfer, Transfer};
+use anchor_lang::system_program::{transfer, Transfer, create_account, CreateAccount};
 use anchor_lang::solana_program::{sysvar::fees::Fees, sysvar::rent::Rent ,sysvar::Sysvar};
 use base64::engine::general_purpose::STANDARD;
 use base64::Engine;
@@ -23,7 +23,8 @@ mod solana_gateway {
         let gateway_state = &mut ctx.accounts.gateway_state;
         gateway_state.owner = *ctx.accounts.user.key;
         gateway_state.task_id = 0;
-        gateway_state.tasks = Vec::new();
+        gateway_state.length = 0;
+        gateway_state.page_count = 0;
         Ok(())
     }
 
@@ -40,7 +41,7 @@ mod solana_gateway {
         ctx: Context<Send>,
         payload_hash: [u8; 32],
         user_address: Pubkey,
-        routing_info: String,
+        _routing_info: String,
         execution_info: ExecutionInfo,
     ) -> Result<()> {
         let gateway_state = &mut ctx.accounts.gateway_state;
@@ -48,6 +49,7 @@ mod solana_gateway {
         // Fetch the current lamports per signature cost for the singature 
         let lamports_per_signature = Fees::get().unwrap().fee_calculator.lamports_per_signature;
 
+        //Calculate the rent for extra storage
         let lamports_per_byte_year = Rent::get().unwrap().lamports_per_byte_year;
         
         // Estimate the cost based on the callback gas limit
@@ -88,11 +90,53 @@ mod solana_gateway {
 
         // Persist the task
         let task = Task {
-            payload_hash,
+            payload_hash: payload_hash,
             completed: false,
         };
 
-        gateway_state.tasks.push(task);
+        // Determine the page index and element index within the page
+        let page_index = gateway_state.length / ELEMENTS_PER_PAGE as u64;
+        let element_index = (gateway_state.length % ELEMENTS_PER_PAGE as u64) as usize;
+
+
+        // Check if a new page is needed
+        if gateway_state.length % ELEMENTS_PER_PAGE as u64 == 0 {
+
+            // Create a new page account
+            let gateway_key = gateway_state.key();
+            let page_index_bytes = page_index.to_le_bytes();
+
+            let page_seeds = &[
+                b"page",
+                gateway_key.as_ref(),
+                &page_index_bytes,
+            ];
+
+            let page_signer = &[&page_seeds[..]];
+
+            let space = 8 + 8 + std::mem::size_of::<Task>() * ELEMENTS_PER_PAGE;
+            let lamports = Rent::get()?.minimum_balance(space);
+            
+            let cpi_accounts = CreateAccount {
+                from: ctx.accounts.user.to_account_info(),
+                to: ctx.accounts.page_account.to_account_info(),
+            };
+
+            let cpi_context = CpiContext::new_with_signer(
+                ctx.accounts.system_program.to_account_info(),
+                cpi_accounts,
+                page_signer,
+            );
+
+            create_account(cpi_context, lamports, space as u64, &id())?;
+            gateway_state.page_count += 1;
+        }
+
+        let page_account = &mut ctx.accounts.page_account;
+        page_account.elements[element_index] = task;
+        page_account.count += 1;
+
+        gateway_state.length += 1;
 
         emit!(LogNewTask {
             task_id: gateway_state.task_id,
@@ -109,15 +153,23 @@ mod solana_gateway {
     pub fn post_execution(
         ctx: Context<PostExecution>,
         task_id: u64,
-        source_network: String,
+        _source_network: String,
         post_execution_info: PostExecutionInfo,
     ) -> Result<()> {
-        let gateway_state = &mut ctx.accounts.gateway_state;
+        let _gateway_state = &mut ctx.accounts.gateway_state;
 
-        let task = gateway_state
-            .tasks
-            .get_mut(task_id as usize)
-            .ok_or(TaskError::TaskNotFound)?;
+        let _page_index = task_id / ELEMENTS_PER_PAGE as u64;
+        let element_index = (task_id % ELEMENTS_PER_PAGE as u64) as usize;
+    
+        // Fetch the page account
+        let page_account = &ctx.accounts.page_account;
+    
+        // Retrieve the task from the page
+        let mut task = page_account
+            .elements
+            .get(element_index)
+            .ok_or(TaskError::TaskNotFound)?
+            .clone();
 
         // Check if the task is already completed
         require!(!task.completed, TaskError::TaskAlreadyCompleted);
@@ -172,21 +224,27 @@ mod solana_gateway {
         task.completed = true;
 
         let callback_data = CallbackData {
-            callback_selector: post_execution_info.callback_selector,
+            callback_selector: post_execution_info.callback_selector.clone(),
             task_id: task_id,
             result: post_execution_info.result,
         };
 
         let borsh_data = callback_data.try_to_vec().unwrap();
 
+        // Convert the String to a Pubkey
+        let callback_address_pubkey = Pubkey::try_from(post_execution_info.callback_address.as_str())
+        .expect("Invalid Pubkey for callback address");
+        let callback_selector_pubkey = Pubkey::try_from(post_execution_info.callback_selector.as_str())
+        .expect("Invalid Pubkey for callback selector");
+       
         // Execute the callback
         let callback_result = invoke(
             &Instruction {
-                program_id: ctx.accounts.callback_program.key(),
-                accounts: vec![AccountMeta::new(ctx.accounts.callback_address.key(), false)],
+                program_id: callback_selector_pubkey,
+                accounts: vec![AccountMeta::new(callback_address_pubkey, false)],
                 data: borsh_data,
             },
-            &[ctx.accounts.callback_address.clone()],
+            &[],
         );
 
         emit!(TaskCompleted {
@@ -200,7 +258,7 @@ mod solana_gateway {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 5000)]
+    #[account(init, payer = user, space = 8 + 8 + 8 + 100)]
     pub gateway_state: Account<'info, GatewayState>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -214,29 +272,53 @@ pub struct IncreaseTaskId<'info> {
     pub owner: Signer<'info>,
 }
 
-#[derive(Accounts)]
-pub struct Send<'info> {
-    #[account(mut)]
-    pub gateway_state: Account<'info, GatewayState>,
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-    pub callback_program: Program<'info, System>,
-    pub callback_address: AccountInfo<'info>,
+const ELEMENTS_PER_PAGE: usize = 100; // Adjust based on the element size and 10,000 bytes limit
+
+#[account]
+pub struct Page {
+    pub elements: [Task; ELEMENTS_PER_PAGE],
+    pub count: u64, // Number of elements in the page
 }
 
 #[derive(Accounts)]
+#[instruction(page_index: u64)]
+pub struct Send<'info> {
+    #[account(mut)]
+    pub gateway_state: Account<'info, GatewayState>,
+    #[account(
+        init,
+        payer = user, 
+        space = 8 + 8 + std::mem::size_of::<Task>() * ELEMENTS_PER_PAGE, 
+        seeds = [b"page", gateway_state.key().as_ref(), &page_index.to_le_bytes()], 
+        bump
+    )]
+    pub page_account: Account<'info, Page>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(task_id: u64)]
 pub struct PostExecution<'info> {
     #[account(mut)]
     pub gateway_state: Account<'info, GatewayState>,
-    pub callback_program: Program<'info, System>,
-    pub callback_address: AccountInfo<'info>,
+    #[account(
+        seeds = [b"page", gateway_state.key().as_ref(), &(task_id / ELEMENTS_PER_PAGE as u64).to_le_bytes()],
+        bump
+    )]
+    pub page_account: Account<'info, Page>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct GatewayState {
     pub owner: Pubkey,
     pub task_id: u64,
-    pub tasks: Vec<Task>,
+    pub length: u64,
+    pub page_count: u64,
 }
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
